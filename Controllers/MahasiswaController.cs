a9d1bcbc903604db7d70782e128365a125e987fc
Rouzok <98026745+Rouzok@users.noreply.github.com>
2026-04-27 02:57:02 +0700

    menambahkan API mahasiswa

---
+++ Controllers/MahasiswaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tpmodul9_103082400009.Models;
+
+namespace tpmodul9_103082400009.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { nama = "Akhmad Alvan Hani", nim = "103082400009" },
+            new Mahasiswa { nama = "Amelia Amanda", nim = "103082400039" },
+            new Mahasiswa { nama = "Miratun Nisa", nim = "103082400001" },
+            new Mahasiswa { nama = "Debby Tampubolon", nim = "103082400029" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mahasiswaBaru)
+        {
+            dataMahasiswa.Add(mahasiswaBaru);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿namespace tpmodul9_103082400009.Models
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+
+        public Mahasiswa() { }
+
+        public Mahasiswa(string nama, string nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+        }
+    }
+}
